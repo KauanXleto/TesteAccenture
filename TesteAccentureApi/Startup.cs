a97c8ebdbd88b2e3197b0c3e34a9d9e210ff15da
Kauan <kauan@gclaims.com.br>
2023-02-13 13:57:09 -0300

    Correção StartUp

--- TesteAccentureApi/Startup.cs
+++ TesteAccentureApi/Startup.cs
@@ -69,9 +69,10 @@ namespace TesteAccentureApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TesteAccentureApi v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TesteAccentureApi v1"));
 
             app.UseCors(x => x
                .AllowAnyMethod()
